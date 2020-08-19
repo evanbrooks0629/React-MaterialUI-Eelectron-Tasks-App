68a7d40fe573da4add5016a0c035c2305fad08b9
evanbrooks0629 <60624108+evanbrooks0629@users.noreply.github.com>
2020-08-18 21:53:29 -0400

    Add files via upload

---
+++ src/theme.js
@@ -0,0 +1,16 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+// import lightBlue from '@material-ui/core/colors/lightBlue';
+//import pink from '@material-ui/core/colors/pink';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#ffffff'
+        },
+        secondary: {
+            main: '#9778ce'
+        }
+    }
+});
+
+export default theme;
